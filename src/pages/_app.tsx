18d050ceebbfa3e88847d798f6252bfe9eea19c8
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2024-01-18 02:39:05 +0530

    add `TxnButton` example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { goerli } from "viem/chains";
+import { WagmiProvider, createConfig, http } from "wagmi";
+const queryClient = new QueryClient();
+
+export const wagmiConfig = createConfig({
+  chains: [goerli],
+  transports: {
+    [goerli.id]: http(),
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
